38184f629ee71e502880884d4bde373374116bf1
TravisWongX <travis.x.wong@gmail.com>
2021-08-08 10:37:16 +0800

    login

--- src/App.js
+++ src/App.js
@@ -10,28 +10,49 @@ const App = () => {
   const [password, setPassword] = useState('')
 
   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+
+  useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
   }, [])
 
-  const handleLogin = () => {
-    const user = loginService.login({ username, password })
-    console.log(user)
-    setUser(user)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      window.localStorage.setItem('loggedappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
   }
 
-  const logForm = () => (
+  const loginForm = () => (
     <form onSubmit={handleLogin}>
       username: <input type='text' name='username' onChange={({ target }) => setUsername(target.value)} />
       password: <input type='password' name='password' onChange={(event) => setPassword(event.target.value)} />
-      <button type='submit'>submit</button>
+      <button type='submit'>login</button>
     </form>
   )
 
   return (
     <div>
-      {user === null && logForm()}
+      {user === null && loginForm()}
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
