a8e325db6fda13a21d1ec0d79ef4564bacb5403b
TravisWongX <travis.x.wong@gmail.com>
2021-08-10 13:49:49 +0800

    show or hide blog contents

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const [buttonLabel, setButtonLabel] = useState('view')
+  const showStyle = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+    if (visible) {
+      setButtonLabel('view')
+    } else {
+      setButtonLabel('hide')
+    }
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={showStyle}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
